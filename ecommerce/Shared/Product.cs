ed070dc04b394d1e3353aac590f0c5dae317ec0b
aurelius-web <giatsidis@outlook.com>
2021-08-26 12:33:48 +0300

    first commit

---
+++ ecommerce/Shared/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecommerce.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImage { get; set; } = "https://via.placeholder.com/250x250";
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+
+    }
+}
